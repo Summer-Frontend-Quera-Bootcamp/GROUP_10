34e5bba226e5ecd8f2680055121d67b3e6ee5c52
Armaan Seydi <armaanseydi@gmail.com>
2023-09-20 23:28:37 +0330

    cleaned up some code

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,9 @@
 import { PropsWithChildren } from "react";
 import "./App.css";
 
-//---------- Form component test --------------//
-
-import { AuthFromLogin } from "../Components/Form/AuthFroms";
-
 interface IPropsApp extends PropsWithChildren {}
 const App: React.FC<IPropsApp> = (/*props: IPropsApp*/): JSX.Element => {
-  return <AuthFromLogin />;
+  return <div></div>;
 };
 
 export default App;
