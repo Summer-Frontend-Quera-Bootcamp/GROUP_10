2cc85934ae97516691fbc7eb0aa1a37023612d2b
amirabdolazimi <amirabdolazimi.v@gmail.com>
2023-09-23 06:03:48 +0330

    Register file & folder name changed

--- src/Pages/AuthenticationPages/index.tsx
+++ src/Pages/AuthenticationPages/index.tsx
@@ -1,6 +1,6 @@
 import ForgetPassPage from "./ForgetPasswordPage/ForgetPasswordPage";
 import LoginPage from "./LoginPage/LoginPage";
 import ResetPasswordPage from "./ResetPasswordPage/ResetPasswordPage";
-import Register from "./Register/Register";
+import Register from "./RegisterPage/RegisterPage";
 
 export { ForgetPassPage, LoginPage, ResetPasswordPage, Register };
