fcfff0369ce77dfe1a3fdf8f5c53e17264cb1fd4
Armaan Seydi <armaanseydi@gmail.com>
2023-09-23 17:22:39 +0330

    finishing touches to Registery

--- src/Pages/AuthenticationPages/RegisterPage/RegisterPage.tsx
+++ src/Pages/AuthenticationPages/RegisterPage/RegisterPage.tsx
@@ -6,6 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 //------ Other Components -----//
 
+import AuthLayout from "../../../Layouts/AuthLayout";
 import { ButtonPrimary } from "../../../Components/Ui/Buttons";
 import { ContainerAuth } from "../../../Components/Ui/Containers";
 import { InputText } from "../../../Components/Ui/Inputs";
@@ -46,75 +47,77 @@ const Register = () => {
   //----------- Return -----------//
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <ContainerAuth heading=" ثبت‌نام در کوئرا تسک منیجر ">
-        <div className="flex flex-col">
-          <div>
-            <InputText
-              name="username"
-              register={register}
-              label="نام کامل"
-              type="text"
-            />
-            {errors.username && (
+    <AuthLayout>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <ContainerAuth heading=" ثبت‌نام در کوئرا تسک منیجر ">
+          <div className="flex flex-col">
+            <div>
+              <InputText
+                name="username"
+                register={register}
+                label="نام کامل"
+                type="text"
+              />
+              {errors.username && (
+                <p className="text-red-primary text-right">
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
+            <div className="mt-m">
+              <InputText
+                name="email"
+                register={register}
+                label="ایمیل"
+                type="text"
+              />
+              {errors.email && (
+                <p className="text-red-primary text-right">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
+            <div className="my-m">
+              <InputText
+                name="password"
+                register={register}
+                label="رمز عبور"
+                type="password"
+              />
+              {errors.password && (
+                <p className="text-red-primary text-right">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            <div dir="rtl" className="flex items-center">
+              <input
+                type="checkBox"
+                id="guideline"
+                className="w-m h-m cursor-pointer rounded"
+                {...register("guideline")}
+              />
+              <label
+                htmlFor="guideline"
+                className="mx-xs font-bold cursor-pointer"
+              >
+                قوانین و مقررات را می‌پذیرم.
+              </label>
+            </div>
+            {errors.guideline && (
               <p className="text-red-primary text-right">
-                {errors.username.message}
+                {errors.guideline.message}
               </p>
             )}
+            <div className="mt-m">
+              <ButtonPrimary onClick={() => {}} bigger={true} type="submit">
+                ثبت‌نام
+              </ButtonPrimary>
+            </div>
           </div>
-          <div className="mt-m">
-            <InputText
-              name="email"
-              register={register}
-              label="ایمیل"
-              type="text"
-            />
-            {errors.email && (
-              <p className="text-red-primary text-right">
-                {errors.email.message}
-              </p>
-            )}
-          </div>
-          <div className="my-m">
-            <InputText
-              name="password"
-              register={register}
-              label="رمز عبور"
-              type="password"
-            />
-            {errors.password && (
-              <p className="text-red-primary text-right">
-                {errors.password.message}
-              </p>
-            )}
-          </div>
-          <div dir="rtl" className="flex items-center">
-            <input
-              type="checkBox"
-              id="guideline"
-              className="w-m h-m cursor-pointer rounded"
-              {...register("guideline")}
-            />
-            <label
-              htmlFor="guideline"
-              className="mx-xs font-bold cursor-pointer"
-            >
-              قوانین و مقررات را می‌پذیرم.
-            </label>
-          </div>
-          {errors.guideline && (
-            <p className="text-red-primary text-right">
-              {errors.guideline.message}
-            </p>
-          )}
-          <div className="mt-m">
-            <ButtonPrimary onClick={() => {}} bigger={true} type="submit">
-              ثبت‌نام
-            </ButtonPrimary>
-          </div>
-        </div>
-      </ContainerAuth>
-    </form>
+        </ContainerAuth>
+      </form>
+    </AuthLayout>
   );
 };
 
