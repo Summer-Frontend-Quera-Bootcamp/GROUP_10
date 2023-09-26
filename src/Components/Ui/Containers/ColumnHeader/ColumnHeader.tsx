a256b0bd38eb8bca3b7e7f0f283831dbb419483d
Armaan Seydi <armaanseydi@gmail.com>
2023-09-26 20:18:56 +0330

    added the more pop up to columnHeader

--- src/Components/Ui/Containers/ColumnHeader/ColumnHeader.tsx
+++ src/Components/Ui/Containers/ColumnHeader/ColumnHeader.tsx
@@ -1,20 +1,36 @@
 import "./styles.css";
+import { useState } from "react";
 
 import { FaPlus } from "react-icons/fa6";
 import { TfiMoreAlt } from "react-icons/tfi";
+import { FiEdit } from "react-icons/fi";
+import { PiArchiveTray } from "react-icons/pi";
+import { GoTrash } from "react-icons/go";
 interface IColumnHeaderProps {
+  color: string;
   name: string;
   count: number;
   onAdd: () => void;
-  onMore: () => void;
+  onEdit: () => void;
+  onArchive: () => void;
+  onDelete: () => void;
 }
 
 export const ColumnHeader = ({
   name,
   count,
   onAdd,
-  onMore,
+  onArchive,
+  onDelete,
+  onEdit,
 }: IColumnHeaderProps) => {
+  const [moreDisplay, setMoreDisplay] = useState(false);
+
+  const handleMoreClick = () => {
+    console.log("click");
+    setMoreDisplay(true);
+  };
+
   return (
     <div className="w-full h-[44px] bg-blue-primary relative rounded-2xl shadow-xl">
       <div
@@ -26,7 +42,7 @@ export const ColumnHeader = ({
           className=" flex gap-xs text-gray-darker transition-opacity duration-300 ease-in-out"
         >
           <FaPlus onClick={onAdd} className="cursor-pointer" />
-          <TfiMoreAlt onClick={onMore} className="cursor-pointer" />
+          <TfiMoreAlt onClick={handleMoreClick} className="cursor-pointer" />
         </div>
         <div className="flex gap-xs ml-auto">
           <span className="bg-gray-secondary text-lg px-xs rounded-full">
@@ -35,6 +51,36 @@ export const ColumnHeader = ({
           <p className="font-bold text-lg">{name}</p>
         </div>
       </div>
+      {moreDisplay && (
+        <div className="bg-gray-secondary w-[160px] h-[150px] absolute rounded-md left-[15px] top-[15px] shadow-xl text-right text-sm flex flex-col">
+          <button
+            onClick={onEdit}
+            className="flex flex-row-reverse items-center gap-xs w-full rounded-tr-md rounded-tl-md h-1/4 hover:bg-gray-300 px-xs"
+          >
+            <FiEdit size="19" /> ویرایش نام ستون
+          </button>
+          <button
+            onClick={onAdd}
+            className="flex flex-row-reverse items-center gap-xs w-full h-1/4 hover:bg-gray-300 px-xs"
+          >
+            <FaPlus size="16" className="pr-[4px]" /> افزودن تسک
+          </button>
+          <button
+            onClick={onArchive}
+            className="flex flex-row-reverse items-center gap-xs w-full h-1/4 hover:bg-gray-300 px-xs"
+          >
+            <PiArchiveTray size="20" />
+            آرشیو تمام تسک‌ها
+          </button>
+          <button
+            onClick={onDelete}
+            className="flex flex-row-reverse items-center gap-xs w-full rounded-bl-md rounded-br-md h-1/4 hover:bg-gray-300 px-xs"
+          >
+            <GoTrash size="19" />
+            حذف ستون
+          </button>
+        </div>
+      )}
     </div>
   );
 };
