21fb8c7c1eade37d1eb34d7da82e9ba29ca8633e
erabiei68 <e.rabiei68@gmail.com>
2023-10-14 16:45:45 +0330

    changes in AccountInfo and UserInfo

--- src/Components/Form/UserInfo/UserInfo.tsx
+++ src/Components/Form/UserInfo/UserInfo.tsx
@@ -1,23 +1,23 @@
-import { ChangeEvent, useRef, useState } from "react";
+// import { ChangeEvent, useRef, useState } from "react";
 import { ButtonPrimary } from "../../Ui/Buttons";
 
 const UserInfo = () => {
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  // const fileInputRef = useRef<HTMLInputElement | null>(null);
+  // const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-  const handleButtonClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click();
-    }
-  };
+  // const handleButtonClick = () => {
+  //   if (fileInputRef.current) {
+  //     fileInputRef.current.click();
+  //   }
+  // };
 
-  const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      const imgUrl = URL.createObjectURL(file);
-      setImageUrl(imgUrl);
-    }
-  };
+  // const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+  //   const file = e.target.files?.[0];
+  //   if (file) {
+  //     const imgUrl = URL.createObjectURL(file);
+  //     setImageUrl(imgUrl);
+  //   }
+  // };
 
   return (
     <form className="w-[354px] flex flex-col items-end gap-s">
@@ -25,32 +25,33 @@ const UserInfo = () => {
       <div className="flex flex-row justify-start w-[328px] gap-xs">
         <div className="flex flex-col justify-center items-center gap-s">
           <button
-            onClick={handleButtonClick}
+            onClick={() => {}}
             className="border border-cyan-primary rounded-lg p-[10px] text-cyan-primary"
           >
             ویرایش تصویر پروفایل
           </button>
-          <input
+          {/* <input
             type="file"
             accept="image/*"
-            ref={fileInputRef}
-            onChange={handleImageChange}
+            ref={}
+            onChange={}
             className="hidden"
-          />
+          /> */}
           <p className="text-[12px] text-right text-gray-primary">
             این تصویر برای عموم قابل نمایش است
           </p>
         </div>
-        {imageUrl ? (
+        {/* {imageUrl ? (
           <div
-            className="bg-cover bg-center bg-orange-secondary h-[104px] w-[100px] rounded-full"
+            className="bg-cover bg-center h-[104px] w-[100px] rounded-full"
             style={{
               backgroundImage: `url(${imageUrl})`,
             }}
           />
         ) : (
-          <div className="bg-cover bg-center bg-orange-secondary h-[104px] w-[100px] rounded-full" />
-        )}
+          <div className=" bg-center bg-orange-secondary h-[104px] w-[100px] rounded-full" />
+        )} */}
+        <div className="bg-cover bg-center bg-[url('../src/assets/prof.jpg')] bg-orange-secondary h-[104px] w-[100px] rounded-full" />
       </div>
       <div className="flex flex-col text-right h-[72px] gap-xs w-full">
         <label htmlFor="firstName">نام</label>
